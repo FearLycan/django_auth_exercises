8f23ed7396e11dc740c46932917c0800f3e04525
Damian Brończyk <damian.bronczyk@tauceti.email>
2018-07-06 15:25:27 +0200

    use generic views

--- user/urls.py
+++ user/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('user', views.user_view, name='user'),
     path('register', views.register_view, name='register'),
     path('login', views.login_view, name='login'),
--- user/views.py
+++ user/views.py
@@ -3,12 +3,22 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.views import generic
 
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'user/index.html')
+class IndexView(generic.ListView):
+    template_name = 'user/index.html'
+    context_object_name = 'user_list'
+
+    def get_queryset(self):
+        """Return five users."""
+        return User.objects.all()[:5]
+
+
+# def index(request):
+#   return render(request, 'user/index.html')
 
 
 def register_view(request):
